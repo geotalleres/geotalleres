98df0e726a9d5a195ad9c1690afe4ea1b4b7b963
Fernando González <fernando.gonzalez@geomati.co>
2014-02-18 18:33:43 +0100

    Eliminado el script para subir a mi FTP. Añadido script para actualizar gh-pages

---
+++ update-github.io.sh
@@ -0,0 +1,12 @@
+cd /tmp
+git clone git@github.com:geotalleres/geotalleres.git
+cd geotalleres
+git submodule update --init
+make html
+ls | grep -v build | xargs rm -fr
+git checkout gh-pages 
+cp -R build/html/* .
+rm -fr build/
+git add -A
+git commit -m "actualizacion"
+git push
